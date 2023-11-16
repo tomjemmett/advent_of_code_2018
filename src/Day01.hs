19a7ae82f0c872362abcc4234fbe2eca29657e89
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-16 20:47:55 +0000

    refactors to use state monad

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,15 +2,26 @@ module Day01 (day01) where
 
 import Common
 import Data.HashSet qualified as S
+import Control.Monad.State (State (..), execState, get, put)
 
 day01 :: AOCSolution
-day01 input = show <$> [sum i, part2 (cycle i) 0 S.empty]
+day01 input = show <$> [sum i, execState (part2 (cycle i) S.empty) 0]
   where
     i :: [Int]
-    i = map p $ lines input
+    i = parseInput input
+
+parseInput :: String -> [Int]
+parseInput = map p . lines
+  where
     p :: String -> Int
     p ('+':xs) = p xs
     p xs = read xs
 
-part2 :: [Int] -> Int -> S.HashSet Int -> Int
-part2 (x:xs) n ys = if n `S.member` ys then n else part2 xs (x + n) (S.insert n ys)
+part2 :: [Int] -> S.HashSet Int -> State Int ()
+part2 (x:xs) ys = do
+  s <- get
+  if s `S.member` ys
+    then pure ()
+    else do
+      put $ s + x
+      part2 xs $ S.insert s ys
