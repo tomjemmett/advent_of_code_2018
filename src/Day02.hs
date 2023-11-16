f1791c41079c4621602bdee6109c0af2e4255dd3
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-16 22:29:30 +0000

    adds day 2 solution

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,31 @@
-module Day02 where -- (day02) where
+module Day02 (day02) where
 
 import Common
+import Data.List (find, transpose)
+import Data.HashMap.Strict qualified as M
 
 day02 :: AOCSolution
-day02 input = []
+day02 input = [part1, part2] <*> pure (lines input)
+
+part1 :: [String] -> String
+part1 = show . uncurry (*) . tuplify2 . map (countTrue id) . transpose . map f . parseInput
+  where
+    parseInput :: [String] -> [M.HashMap Char Int]
+    parseInput = map (foldr (flip (M.insertWith (+)) 1) M.empty)
+    f :: M.HashMap Char Int -> [Bool]
+    f x = (`elem` x) <$> [2, 3]
+
+part2 :: [String] -> String
+part2 (x:xs) = case find ((==l) . succ . length) m of
+    Just r -> r
+    _ -> part2 xs
+  where
+    l = length x
+    m = map (match x) xs
+
+match :: Eq a => [a] -> [a] -> [a]
+match [] _ = []
+match (a:as) (b:bs) = v
+  where
+    v = if a == b then a:r else r
+    r = match as bs
--- test/Day02Spec.hs
+++ test/Day02Spec.hs
@@ -2,18 +2,13 @@ module Day02Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 2" $ do
-  it "Sample" $ do
-    day02 testInput `shouldBe` []
-
   it "Actual" $ do
     withFile
       "inputs/day02.txt"
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day02 actualInput `shouldBe` []
+          day02 actualInput `shouldBe` ["6225", "revtaubfniyhsgxdoajwkqilp"]
       )
