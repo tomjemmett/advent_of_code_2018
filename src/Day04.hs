e1080ff79703e3cb188f6db89255665610e5a545
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-17 14:06:54 +0000

    adds day 4 solution

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,6 +1,62 @@
-module Day04 where -- (day04) where
+module Day04 (day04) where
 
 import Common
+import Data.Function (on)
+import Data.List (sort, maximumBy, elemIndex)
+import Data.Maybe (fromJust)
+import Data.Time
+    ( UTCTime(utctDayTime),
+      defaultTimeLocale,
+      parseTimeOrError,
+      diffUTCTime )
+import Data.HashMap.Strict qualified as M
+import Text.Parsec qualified as P
+
+data Log = GuardStart Int UTCTime | FallsAsleep UTCTime | WakesUp UTCTime deriving (Show)
 
 day04 :: AOCSolution
-day04 input = []
+day04 input = show . answer r <$> [sum, maximum]
+  where
+    i = parseInput input
+    r = go M.empty 0 i
+
+parseInput :: String -> [Log]
+parseInput = map parseLine . sort . lines
+
+parseLine :: String -> Log
+parseLine = parse' p id
+  where
+    p = do
+      P.char '['
+      t <- parseTime <$> P.manyTill P.anyChar (P.string "] ")
+      f <- P.choice $ P.try <$> [pGuard, pFallsAsleep, pWakesUp]
+      pure $ f t
+    pGuard = GuardStart <$> (P.string "Guard #" *> number)
+    pFallsAsleep = FallsAsleep <$ P.string "falls asleep"
+    pWakesUp = WakesUp <$ P.string "wakes up"
+
+parseTime :: String -> UTCTime
+parseTime = parseTimeOrError True defaultTimeLocale "%Y-%m-%d %H:%M" 
+
+answer :: [(Int, [Int])] -> ([Int] -> Int) -> Int
+answer r f = g * t
+  where
+    (g, getMaxTime -> t) = maximumBy (compare `on` f . snd) r
+
+getMaxTime :: [Int] -> Int
+getMaxTime r = fromJust $ maximum r `elemIndex` r
+
+go :: M.HashMap Int [Int] -> Int -> [Log] -> [(Int, [Int])]
+go s _ [] = M.toList s
+go s _ (GuardStart g _:xs) = go s g xs
+go s g (FallsAsleep a:WakesUp b:xs) = go s' g xs
+  where
+    d = floor (diffUTCTime b a) `div` 60
+    a' = floor (utctDayTime a) `div` 60
+    t = [a' + i | i <- [0..d-1]]
+    v = M.lookupDefault initTimes g s
+    v' = zipWith (+) v [fromEnum (i >= a' && i < (a' + d)) | i <- [0..59]]
+    s' = M.insert g v' s
+
+initTimes :: [Int]
+initTimes = repeat 0
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -2,12 +2,29 @@ module Day04Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput = 
+  "[1518-11-01 00:00] Guard #10 begins shift\n\
+  \[1518-11-01 00:05] falls asleep\n\
+  \[1518-11-01 00:25] wakes up\n\
+  \[1518-11-01 00:30] falls asleep\n\
+  \[1518-11-01 00:55] wakes up\n\
+  \[1518-11-01 23:58] Guard #99 begins shift\n\
+  \[1518-11-02 00:40] falls asleep\n\
+  \[1518-11-02 00:50] wakes up\n\
+  \[1518-11-03 00:05] Guard #10 begins shift\n\
+  \[1518-11-03 00:24] falls asleep\n\
+  \[1518-11-03 00:29] wakes up\n\
+  \[1518-11-04 00:02] Guard #99 begins shift\n\
+  \[1518-11-04 00:36] falls asleep\n\
+  \[1518-11-04 00:46] wakes up\n\
+  \[1518-11-05 00:03] Guard #99 begins shift\n\
+  \[1518-11-05 00:45] falls asleep\n\
+  \[1518-11-05 00:55] wakes up"
 
 spec :: Spec
 spec = describe "Day 4" $ do
   it "Sample" $ do
-    day04 testInput `shouldBe` []
+    day04 testInput `shouldBe` ["240", "4455"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +32,5 @@ spec = describe "Day 4" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day04 actualInput `shouldBe` []
+          day04 actualInput `shouldBe` ["73646", "4727"]
       )
