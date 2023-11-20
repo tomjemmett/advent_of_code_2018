b85d9de5386642667a3fb7a00f87ba5cdb717638
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-20 14:18:01 +0000

    adds day 9 solution

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,6 +1,33 @@
-module Day09 where -- (day09) where
+module Day09 (day09) where
 
 import Common
+import Data.Maybe (fromJust)
+import Data.List.PointedList.Circular ( PointedList(_focus))
+import Data.List.PointedList.Circular qualified as PL
+import Data.Vector.Unboxed qualified as V
+import Control.Lens ( (&), (+~), Ixed(ix) )
+import Data.List(foldl')
 
 day09 :: AOCSolution
-day09 input = []
+day09 input = show . go n <$> ((*) <$> [1, 100] <*> pure m)
+  where
+    (n, m) = parseInput input
+
+parseInput :: String -> (Int, Int)
+parseInput i = tuplify2 $ read <$> ([(!!0), (!!6)] <*> pure (words i))
+
+go :: Int -> Int -> Int
+go n m = V.maximum . fst . foldl' f (V.replicate n 0, PL.singleton 0) $ i
+  where
+    f (!s, !g) (!p, !x) = (s & ix p +~ pts, g')
+      where
+        (pts, g') = place x g
+    i = zip ((`mod` n) <$> [0..]) [1..m]
+
+place :: Int -> PointedList Int -> (Int, PointedList Int)
+place x l
+    | x `mod` 23 == 0 = (_focus l' + x, fromJust (PL.deleteRight l'))
+    | otherwise = (0, (PL.insertLeft x . PL.moveN 2) l)
+    where
+      l' = PL.moveN (-7) l
+
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -2,12 +2,12 @@ module Day09Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "10 players; last marble is worth 1618 points"
 
 spec :: Spec
 spec = describe "Day 9" $ do
   it "Sample" $ do
-    day09 testInput `shouldBe` []
+    day09 testInput `shouldBe` ["8317", "74765078"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +15,5 @@ spec = describe "Day 9" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day09 actualInput `shouldBe` []
+          day09 actualInput `shouldBe` ["370210", "3101176548"]
       )
