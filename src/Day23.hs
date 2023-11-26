71b48b34f267477a667c0132d2391ecec09d36b1
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-26 11:47:10 +0000

    adds day 23 solution

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,6 +1,51 @@
-module Day23 where -- (day23) where
+module Day23 (day23) where
 
 import Common
+import Control.Monad (when)
+import Control.Monad.State
+import Data.Function (on)
+import Data.List (foldl', maximumBy, sort, find)
+import Data.HashMap.Strict qualified as M
+import Text.Parsec qualified as P
 
 day23 :: AOCSolution
-day23 input = []
+day23 input = show <$> ([part1, part2] <*> pure (parseInput input))
+
+part1 :: [(Point3d, Int)] -> Int
+part1 i = foldl' (flip f) 0 i
+  where
+    (pos, range) = maximumBy (compare `on` snd) i
+    f (p, _) = if distance pos p <= range then succ else id
+
+
+part2 :: [(Point3d, Int)] -> Int
+part2 i = pred r
+  where
+    x = sort $
+      M.toList $
+      foldr (uncurry $ M.insertWith (+)) M.empty $
+      concatMap (\(untuplify3 -> p, r) -> [(sum p - r, 1), (sum p + r + 1, -1)]) i
+    --
+    (_, _, ms) = execState (f x) (0, 0, 0)
+    Just r = find (> ms) $ map fst x 
+    f :: [(Int, Int)] -> State (Int, Int, Int) ()
+    f [] = pure ()
+    f ((p, v):xs) = do
+      ((+v) -> r, m, ms) <- get
+      put (r, m, ms)
+      when (r > m) $ do
+        put (r, r, p)
+        pure()
+      f xs
+
+distance :: Point3d -> Point3d -> Int
+distance (x1, y1, z1) (x2, y2, z2) = abs (x1 - x2) + abs (y1 - y2) + abs (z1 - z2)
+
+parseInput :: String -> [(Point3d, Int)]
+parseInput = parse' (p `P.sepBy` P.newline) id
+  where
+    p = do
+      P.string "pos="
+      pos <- (P.char '<' `P.between` P.char '>') (number `P.sepBy` P.char ',')
+      r <- P.string ", r=" *> number
+      pure (tuplify3 pos, r)
--- test/Day23Spec.hs
+++ test/Day23Spec.hs
@@ -2,18 +2,13 @@ module Day23Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 23" $ do
-  it "Sample" $ do
-    day23 testInput `shouldBe` []
-
   it "Actual" $ do
     withFile
       "inputs/day23.txt"
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day23 actualInput `shouldBe` []
+          day23 actualInput `shouldBe` ["396", "119406340"]
       )
